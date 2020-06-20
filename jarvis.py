7e0e0ca9ddefe0012bca3e253b3b030e4402d136
Ks121099 <kunalsinha_bt2k17@dtu.ac.in>
2020-06-21 01:14:29 +0530

    search and ss features

--- jarvis.py
+++ jarvis.py
@@ -1,6 +1,9 @@
 import pyttsx3
 import datetime
 import speech_recognition as sr
+import webbrowser as wb
+import pyautogui
+import os
 
 engine = pyttsx3.init()
 month = ["January", "February", "March", "April", "May", "June",
@@ -23,28 +26,65 @@ def date():
     d = str(datetime.datetime.now().day)
     speak("Today's Date is " + d+" "+month[m-1]+" "+y)
 
-# def command():
-#     r=sr.Recognizer
-#     with sr.Microphone as source:
-#         print("Listening.........")
 
-def main():
-    speak("Welcome Back Sir!!")
-    date()
-    time()
+def greeting():
     h = datetime.datetime.now().hour
     if h > 4 and h <= 12:
         greet = " Good Morning Sir"
     elif h > 12 and h <= 16:
         greet = " Good Afternoon Sir"
     elif h > 16 and h <= 21:
-        greet = " Good Evening Sir" 
+        greet = " Good Evening Sir"
     elif h > 21 and h <= 24:
         greet = " Good Night Sir"
     else:
         greet = " Good Night Sir"
+    speak(greet+" , ,Jarvis at your service, Please tell me how can I help you?")
 
-    speak(greet+"Jarvis at your service, Please tell me how can I help you?")
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.........")
+        r.pause_threshold = 1  # wait time
+        audio = r.listen(source=source)
+    try:
+        print("Recognizing.......")
+        query = r.recognize_google(audio, language='en-in')
 
+    except Exception as e:
+        print(e)
+        speak("Say that again please")
+        return "None"
+    return query
 
-main()
+def ss():
+    pic=pyautogui.screenshot()
+    pic.save(r"C:\Users\Admin\Desktop\webd\projects\Jarvis\\screenshot.png")
+
+if __name__ == "__main__":
+    greeting()
+    while True:
+        ip = command().lower()
+        if 'time' in ip:
+            time()
+        elif 'date' in ip:
+            date()
+        elif 'chrome' in ip:
+            speak("Sure sir, But which website should I open in google chrome")
+            path1= 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+            ip1= command().lower()
+            wb.get(path1).open_new_tab(ip1+'.com')
+        elif 'screenshot' in ip:
+            ss()
+            speak("Its Done!! The screenshot has been saved in the respective folder")
+        elif 'logout'in ip:
+            os.system("shutdown -l")
+        elif 'shutdown'in ip:
+            os.system("shutdown /s /t 1")
+        elif 'restart'in ip:
+            os.system("shutdown /r /t 1")
+        elif 'offline' in ip:
+            speak("Ok sir, as you say , I am Going offline")
+            quit()
+
+        speak("What else can I do for you sir?")
